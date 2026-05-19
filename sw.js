4f7fb536c6d4353aea087d51143ce1e1e76d9faf
JamesCriss <james.crispro@gmail.com>
2026-05-19 21:39:03 +0200

    Initial commit: Interval Timer PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'interval-timer-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?q=80&w=1920&auto=format&fit=crop', // Forest / Break
+    'https://images.unsplash.com/photo-1581287053822-fd7bf4f4bfec?q=80&w=1920&auto=format&fit=crop'  // Workshop / Work
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
+});
